6cdaba83c01eb239310efd7d183603672d659e0f
Himzo Hasak <hasak97@hotmail.com>
2023-06-27 19:51:52 +0200

    SetBaudRate

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+serial.setBaudRate(BaudRate.BaudRate9600)
 basic.forever(function () {
     custom.sendSerial("compass", "" + (input.compassHeading()))
     custom.sendSerialWithMinInterval("temp", "" + (input.temperature()))
